472d1b49b3df7f5bef69bc257310ab3b3e08ee37
Tim Daubenschütz <tim@daubenschuetz.de>
2022-01-02 17:10:54 +0100

    Functional demo

--- src/Tickets.sol
+++ src/Tickets.sol
@@ -1,30 +1,60 @@
 // @format
+// SPDX-License-Identifier: GPL-3.0-only
+// Copyright (c) Tim Daubenschütz.
 pragma solidity ^0.8.6;
 
 import "indexed-sparse-merkle-tree/StateTree.sol";
 
 contract Tickets {
-    bytes32 public root;
+    mapping(address => bytes32) public roots;
+    mapping(address => uint256) public prices;
+    mapping(address => uint256) public indexes;
 
-    constructor(address _owner) {
-        root = StateTree.empty();
+    function init(uint256 _price) external {
+        require(roots[msg.sender] == 0x0, "already initialized");
+        roots[msg.sender] = StateTree.empty();
+        prices[msg.sender] = _price;
     }
 
-    function enter(
-        string calldata _entry,
-        bytes32[] calldata _proofs,
-        uint8 _bits
-    ) public payable {
-        require(msg.value > 0);
-        root = StateTree.write(
+    function withdraw() external {
+        uint256 balance = getBalance();
+        require(balance > 0);
+        payable(msg.sender).transfer(balance);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return prices[msg.sender] * indexes[msg.sender];
+    }
+
+    function buy(
+		bytes32[] calldata _proofs,
+        address _provider
+    ) external payable {
+        require(msg.value >= prices[_provider], "not enough eth");
+        uint256 index = indexes[_provider];
+        roots[_provider] = StateTree.write(
             _proofs,
-            _bits,
-            size,
-            keccak256(abi.encode(_entry)),
-            0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563,
-            root
+            (index == 0 ? 0 : StateTree.bitmap(index)),
+            index,
+            keccak256(abi.encode(msg.sender)),
+            0,
+            roots[_provider]
         );
-        size += 1;
-        emit Entry(msg.sender, _entry, msg.value);
+        indexes[_provider] += 1;
     }
+
+	function validate(
+		bytes32[] calldata _proofs,
+      	uint256 _index,
+        address _buyer,
+        address _provider
+	) external view returns (bool) {
+        return StateTree.validate(
+            _proofs,
+            (_index == 0 ? 0 : StateTree.bitmap(_index)),
+            _index,
+            keccak256(abi.encode(_buyer)),
+            roots[_provider]
+        );
+	}
 }
--- src/Tickets.t.sol
+++ src/Tickets.t.sol
@@ -1,14 +1,55 @@
 // @format
+// SPDX-License-Identifier: GPL-3.0-only
+// Copyright (c) Tim Daubenschütz.
 pragma solidity ^0.8.6;
 import "ds-test/test.sol";
 
 import "./Tickets.sol";
+import "indexed-sparse-merkle-tree/StateTree.sol";
 
 contract TicketsTest is DSTest {
     Tickets t;
 
     receive() external payable { }
+
     function setUp() public {
-        t = new Tickets(address(this));
+        t = new Tickets();
+    }
+
+    function testInit() external {
+        uint256 price = 1;
+        t.init(price);
+        assertEq(t.roots(address(this)), StateTree.empty());
+        assertEq(t.prices(address(this)), price);
+    }
+
+    function testBuyAndValidate() external {
+        uint256 price = 1;
+        t.init(price);
+        assertEq(t.roots(address(this)), StateTree.empty());
+		bytes32[] memory proofs = new bytes32[](0);
+        t.buy{value: 1}(proofs, address(this));
+        assertEq(t.indexes(address(this)), 1);
+		bytes32[] memory proofs2 = new bytes32[](1);
+        proofs2[0] = keccak256(abi.encode(address(this)));
+        assertTrue(t.validate(proofs2, 0, address(this), address(this)), "resulting tree is false");
+    }
+
+    function testBalanceAndWithdrawing() external {
+        uint256 price = 1;
+        t.init(price);
+        assertEq(t.roots(address(this)), StateTree.empty());
+		bytes32[] memory proofs = new bytes32[](0);
+        t.buy{value: 1}(proofs, address(this));
+        assertEq(t.indexes(address(this)), 1);
+		bytes32[] memory proofs2 = new bytes32[](1);
+        proofs2[0] = keccak256(abi.encode(address(this)));
+        assertTrue(t.validate(proofs2, 0, address(this), address(this)), "resulting tree is false");
+
+        assertEq(address(t).balance, 1);
+        assertEq(t.indexes(address(this)), 1);
+        assertEq(t.prices(address(this)), 1);
+        t.withdraw();
+        assertEq(address(t).balance, 0);
     }
 }
